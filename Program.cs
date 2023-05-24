0d273173320c897c3facc6208a0790ebe72a8be6
Jennifer <jenniferrowland1987@gmail.com>
2023-05-24 14:37:06 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,92 @@
 ﻿
 using Roshambo;
 
-
 HumanPlayer player = new HumanPlayer();
+RoshamboEnum computerThrow;
+RoshamboEnum playerThrow;
+string userThrowSelection;
+string userOpponentSelection;
+int playerScore = 0;
+int computerScore = 0;
+
+static void DisplayOpponents()
+{
+    Console.WriteLine("Please choose your opponent!");
+    Console.WriteLine("R - Rock Player, C - Computer Player");
+}
+
+static void DisplayMenu(Type enumType)
+{
+    foreach (int i in Enum.GetValues(enumType))
+    {
+        Console.WriteLine($"{Enum.GetName(enumType, i)}");
+        Console.WriteLine("Please choose your throw, R - Rock, P - Paper, or S - Scissors.");
+    }
+}
+
+static RoshamboEnum GetComputerThrow(string opponentSelection)
+{
+    RockPlayer rockPlayer = new RockPlayer();
+    RandomPlayer randomPlayer = new RandomPlayer();
+    RoshamboEnum compThrow;
+   
+    while (!Validators.ValidateOpponentSelection(opponentSelection))
+    {
+        Console.WriteLine("Sorry, that is an invalid opponent selection, please try again.");
+        DisplayOpponents();
+        string input = Console.ReadLine();
+        opponentSelection = input.ToLower().Trim();
+    }
+    switch (opponentSelection)
+    {
+        case "r":
+            compThrow = rockPlayer.GenerateRoshambo();
+            break;
+        case "rock":
+            compThrow = rockPlayer.GenerateRoshambo();
+            break;
+        case "c":
+            compThrow = randomPlayer.GenerateRoshambo();
+            break;
+        case "computer":
+            compThrow = randomPlayer.GenerateRoshambo();
+            break;
+    }
+    return compThrow;
+}
+
+static ResultsEnum GetResults(RoshamboEnum playThrow, RoshamboEnum compThrow)
+{
+    if (playThrow == RoshamboEnum.Rock && compThrow == RoshamboEnum.Paper)
+    {
+        return ResultsEnum.lose;
+    }
+    else if (playThrow == RoshamboEnum.Rock && compThrow == RoshamboEnum.Scissors)
+    {
+        return ResultsEnum.win;
+    }
+    else if (playThrow == RoshamboEnum.Paper && compThrow == RoshamboEnum.Rock)
+    {
+        return ResultsEnum.win;
+    }
+    else if (playThrow == RoshamboEnum.Paper && compThrow == RoshamboEnum.Scissors)
+    {
+        return ResultsEnum.lose;
+    }
+    else if (playThrow == RoshamboEnum.Scissors && compThrow == RoshamboEnum.Rock)
+    {
+        return ResultsEnum.lose;
+    }
+    else if (playThrow == RoshamboEnum.Scissors && compThrow == RoshamboEnum.Paper)
+    {
+        return ResultsEnum.win;
+    }
+    else
+    {
+        return ResultsEnum.draw;
+    }
+}
+
 
 string input = "";
 while (input == "") 
@@ -18,4 +102,51 @@ while (input == "")
 }
 player.Name = input;
 
-Console.WriteLine(player.Name);
+Console.WriteLine($"Welcome, {player.Name}!");
+bool playAgain = true;
+while (playAgain)
+{
+    DisplayOpponents();
+    userOpponentSelection = Console.ReadLine().ToLower().Trim();
+
+    DisplayMenu(typeof(RoshamboEnum));
+    userThrowSelection = Console.ReadLine().ToLower().Trim();
+
+    playerThrow = player.GenerateRoshambo();
+    computerThrow = GetComputerThrow(userOpponentSelection);
+    if(GetResults(playerThrow, computerThrow) == ResultsEnum.win)
+    {
+        Console.WriteLine($"{playerThrow} beats {computerThrow}!  {player.Name} wins this round!");
+        playerScore = +1;           
+    }
+    else if(GetResults(playerThrow, computerThrow) == ResultsEnum.lose)
+    {
+        Console.WriteLine($"{computerThrow} beats {playerThrow}, better luck next time, {player.Name}!");
+        computerScore = +1;
+    }
+    else
+    {
+        Console.WriteLine("it's a draw!");
+    }
+    Console.WriteLine($"{player.Name}, you have {playerScore} points! Your opponent has {computerScore}!");
+    Console.WriteLine($"Would you like to play another round, {player.Name}? (Y/N)");
+    string response = Console.ReadLine().ToLower().Trim();
+    while(response != "y" && response != "yes" && response != "n" && response != "no")
+    {
+        Console.WriteLine("it's a yes or no question... would you like to play again? (Y/N)");
+        response = Console.ReadLine().ToLower().Trim();
+    }
+    if(response == "n" || response == "no")
+    {
+        Console.WriteLine("Thank you for playing!");
+        playAgain = false;
+    }
+    else
+    {
+        playAgain = true;
+        continue;
+    }
+
+}
+
+
